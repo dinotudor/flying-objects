f2267909087a36bb83f17b39766dad00883ee96d
Dino Tudor <dinotudor@gmail.com>
2019-04-12 16:26:30 +0200

    corrected src order html, main function and SplashScreen

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function main () {
+
+  const mainSection = document.querySelector('main');
+
+  function buildDom (html) {
+    mainSection.innerHTML = html;
+    return mainSection
+  }
+
+  function buildSplashScreen () {
+    const splashScreen = buildDom(`
+    <section>
+      <h1>Flying Objects</h1>
+      <button>START GAME</button>
+    </sectin>
+      `);
+    
+
+    
+
+
+  }
+
+  buildSplashScreen();
+
+}
+
+window.addEventListener('load', main);
