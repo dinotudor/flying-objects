526c467bcd7e18ae97b8ef7f86bde7537d749ba1
Dino Tudor <dinotudor@gmail.com>
2019-04-14 18:30:33 +0200

    add bullet constructor

--- src/bullet.js
+++ src/bullet.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function Bullet(canvas, x) {
+  this.speed = 6;
+  this.direction = -1;
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext('2d');
+  this.size = 5;
+  this.x = x;
+  this.y = 0 - this.size/2;
+}
+
+Bullet.prototype.draw = function(){
+  this.ctx.fillStyle = 'white';
+  this.ctx.fillRect(this.x, this.y, this.size, this.size);
+}
+
+Bullet.prototype.update = function(){
+  this.y = this.y - this.direction * this.speed
+}
